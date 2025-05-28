8632c23a8fe6c3e926f02e7e441c21cf0a813fa7
Alex Vazquez <101617394+alexvazquez2000@users.noreply.github.com>
2025-05-28 12:10:29 -0600

    Test the elevator more directly

---
+++ src/test/java/com/smx/elevator/bean/ElevatorTest.java
@@ -0,0 +1,26 @@
+package com.smx.elevator.bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test the Elevator bean
+ * 
+ * @author Alex Vazquez <vazqueza2000@gmail.com>
+ */
+class ElevatorTest {
+
+	@Test
+	void test() {
+		Elevator elevator = new Elevator(0);
+		assertEquals("0 0", elevator.getStatus());
+		elevator.moveElevatorToFloor(0);
+		//it should not change with the same floor
+		assertEquals("0 0", elevator.getStatus());
+		//move to a new floor
+		elevator.moveElevatorToFloor(10);
+		assertEquals("100 0,10", elevator.getStatus());
+	}
+
+}
